5e4949c3367d881ae111ec8816e1ad6c03a358ca
GwakHyeonJun <gwakhyeonjun@gmail.com>
2020-04-21 14:17:34 +0900

    계산기 만들기

--- src/main/java/kr/hs/dgsw/webclass01/HelloController.java
+++ src/main/java/kr/hs/dgsw/webclass01/HelloController.java
@@ -1,12 +1,53 @@
 package kr.hs.dgsw.webclass01;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
     @GetMapping
     public String hello() {
         return "hello world";
+    }
+
+    @GetMapping("/cal1")
+    public int cal1(@RequestParam int num1, @RequestParam int num2, @RequestParam String oper) {
+        int res = 0;
+        switch (oper) {
+            case "+":
+                res = num1 + num2;
+                break;
+            case "-":
+                res = num1 - num2;
+                break;
+            case "*":
+                res = num1 * num2;
+                break;
+            case "/":
+                res = num1 / num2;
+                break;
+        }
+
+        return res;
+    }
+
+    @GetMapping("/cal2/{n1}/{oper}/{n2}")
+    public int cal2(@PathVariable String n1, @PathVariable String n2, @PathVariable String oper) {
+        int res = 0;
+        switch (oper) {
+            case "+":
+                res = Integer.parseInt(n1) + Integer.parseInt(n2);
+                break;
+            case "-":
+                res = Integer.parseInt(n1) - Integer.parseInt(n2);
+                break;
+            case "*":
+                res = Integer.parseInt(n1) * Integer.parseInt(n2);
+                break;
+            case "나누기":
+                res = Integer.parseInt(n1) / Integer.parseInt(n2);
+                break;
+        }
+
+        return res;
     }
 }
