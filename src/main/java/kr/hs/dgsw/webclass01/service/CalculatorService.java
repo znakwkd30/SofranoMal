3417a07bbfc69796891eb8a98aa4fedcb40c408b
GwakHyeonJun <gwakhyeonjun@gmail.com>
2020-04-30 21:07:27 +0900

    MVC valid

---
+++ src/main/java/kr/hs/dgsw/webclass01/service/CalculatorService.java
@@ -0,0 +1,6 @@
+package kr.hs.dgsw.webclass01.service;
+
+public interface CalculatorService {
+    int calculator(String n1, String n2, String oper);
+    String valid(String n1, String n2, String oper);
+}
